a8df47c32948c5ee11e71bf33ce0842845c95ba3
Sasha-Makar <makarsasha7@gmail.com>
2023-11-30 08:49:50 +0200

    add some code to lesson_4 JS

--- JS_LESSONS/LESSONS/lesson_4/main.js
+++ JS_LESSONS/LESSONS/lesson_4/main.js
@@ -1,1 +1,15 @@
 // alert(22)
+
+let size = +prompt('enter integer',5);
+let diapazone = +prompt('enter number',5);
+
+function randomGenerator(size,diapazone) {
+    let arr = [];
+    for (let i = 0; i < size; i++) {
+        arr[arr.length] = (Math.round(Math.random()*diapazone));
+    }
+    return arr;
+}
+
+let xxx = randomGenerator(size,diapazone);
+console.log(xxx);
