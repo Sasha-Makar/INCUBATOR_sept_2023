ce22859842d1a6642ab0d6618c8fd66bb349cf0f
Sasha-Makar <makarsasha7@gmail.com>
2023-12-14 17:01:44 +0200

    filled preview js p_8.1-8.4

--- JS_LESSONS/PREVIEW/p_8.1-8.4/main.js
+++ JS_LESSONS/PREVIEW/p_8.1-8.4/main.js
@@ -15,7 +15,6 @@
 // console.log(user1.age);
 
 
-
 // function User(name, age, status,wife) {
 //     this.name = name;
 //     this.age = age;
@@ -24,7 +23,6 @@
 // }
 // let user = new User('qwe',111,true,{name:'olya',age:777,status:false});
 // console.log(user);
-
 
 
 // function User(name, age, status,wifeName,wifeAge) {
@@ -45,8 +43,186 @@
 // user.greeting();
 
 
+////////////////////-Функції конструктори-////////
 
-////////////-prototype call apply bind-////////////////
+
+// function User(id,name,age) {
+//     this.id = id;
+//     this.name = name;
+//     this.age=age;
+//     // console.log(this);
+// }
+// let user1 = new User(1,'kokos',31);
+// console.log(user1);
+// console.log('-----');
+// new User();
+// console.log('------');
+
+
+// kompozycia
+
+// function User(id,name,age,wife) {
+//     this.id = id;
+//     this.name = name;
+//     this.age=age;
+//     this.wife = wife
+// }
+// let user = new User(1,'kokos',77,{name:'olya',age:31});
+// console.log(user);
+
+
+// agregacia
+
+// function User(id,name,age,wifeName,wifeAge) {
+//     this.id = id;
+//     this.name = name;
+//     this.age=age;
+//     this.wife = {
+//         name:wifeName,
+//         age:wifeAge
+//     }
+//     this.greeting = function (){
+//         console.log('hi');
+//     }
+// }
+//
+// let user = new User(1,'kokos',77,'anna',77);
+// console.log(user);
+// user.greeting();
+
+
+////////////-prototype call apply bind-//////////////// =====>>> on main_2.js
+
+// function User(name,age) {
+//     this.name = name;
+//     this.age = age;
+// }
+//
+// console.log('=================');
+
+
+console.log('====================');
+
+
+// Класи та наслідування
+
+
+// class User {
+//
+//
+//     constructor(name,age) {
+//         this.name = name;
+//         this.age = age;
+//     }
+//     greeting(){
+//         return `hi my name is ${this.name}`
+//     }
+// }
+//
+// let user = new User('kokos',31);
+// console.log(user);
+// console.log(user.greeting());
+//
+// console.log('-----');
+//
+// class UserFabric {
+//     // name,
+//     // age +alt+insert
+//     constructor(name, age) {
+//         this.name = name;
+//         this.age = age;
+//     }
+//     // static
+//     sayHi(){
+//         return `Hi ${this.name}`
+//     }
+// }
+// let user2 = new UserFabric('petya',31);
+// console.log(user2);
+//
+// console.log(user2.sayHi());
+//
+// console.log('======================');
+
+//
+// class User {
+//     constructor(name, age) {
+//         this.name = name;
+//         this.age = age;
+//     }
+//     work(){
+//         return 'in process';
+//     }
+// }
+//
+//
+// class Customer extends User{
+//     constructor(name, age, password) {
+//         super(name, age);
+//         this.password = password;
+//     }
+//
+//     // work() {
+//     //     return super.work();
+//     // }
+//
+//     work() {
+//         return 'fwefwvwgrr';
+//     }
+// }
+// let customer = new Customer('koko',12,'1111111');
+// console.log(customer);
+// console.log(customer.work());
+
+
+function User(name, age) {
+    this.name = name;
+    this.age = age;
+}
+
+
+function Customer(name, age, password) {
+    User.apply(this, arguments);
+    this.password = password;
+}
+
+let customer = new Customer('koko', 111, '7777777');
+
+console.log(customer);
+
+console.log('-------------------------');
+
+
+/////////////////- Прототипи об'єктів-///////////////////
+
+
+console.log({});
+console.log({}.__proto__);
+
+
+let user = {
+    id: 1,
+    name: 'kokos'
+}
+
+
+let user2 = Object.create(user);
+console.log(user2);
+
+console.log(user2 === user);
+
+console.log(user2.id);
+console.log(user2.name);
+
+
+console.log(user2.hasOwnProperty('id'));
+console.log(user2.hasOwnProperty('name'));
+user2.surname = 'qweasdq';
+console.log(user2.hasOwnProperty('surname'));
+console.log(user2);
+console.log('---');
+
+console.log(user.hasOwnProperty('id'));
 
 
 
---
+++ JS_LESSONS/PREVIEW/p_8.1-8.4/main_2.js
@@ -0,0 +1,64 @@
+
+////////////-prototype call apply bind-////////////////
+
+
+// console.log('qweasds');
+//
+// let user = new User('kokos',777);
+// console.log(user);
+//
+//
+// user.greeting = function() {
+//     console.log('hi');
+// }
+//
+// console.log(user);
+// user.greeting();
+//
+// let user1 = new User('koko',78787);
+// console.log(user1);
+//
+// try {
+//     user1.greeting();
+// } catch (e) {
+//     console.log(e);
+// }
+//
+// console.log('==================');
+//
+// // prototype
+//
+// console.log(User);
+//
+// User.prototype.greeting = function (){
+//     return `hello my name is ${this.name}`;
+// };
+//
+// let user2 = new User('kokos',31);
+// console.log(user2);
+// let s = user2.greeting();
+// console.log(s);
+
+//apply
+
+// let user = new User('kokos',777);
+// console.log(user);
+//
+// user.greeting = function() {
+//     return `hi my name is ${this.name}`;
+// };
+//
+// console.log(user.greeting());
+//
+//
+// let user2 = new User('petya',77);
+// console.log(user.greeting.apply(user2));
+//
+// user2.greeting();
+
+
+
+
+
+
+
