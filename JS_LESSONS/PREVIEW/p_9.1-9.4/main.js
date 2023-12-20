c7d01382d15deb008ae108e83e7dbe39e2d84cdf
Sasha-Makar <makarsasha7@gmail.com>
2023-12-20 12:03:41 +0200

    add RECURSION to preview_9.1-9.4

--- JS_LESSONS/PREVIEW/p_9.1-9.4/main.js
+++ JS_LESSONS/PREVIEW/p_9.1-9.4/main.js
@@ -126,9 +126,61 @@
 // }
 
 
-
+// alert(22)
 
 ////Рекурсія додаткове
+
+
+// function explorer(htmlElement) {
+//     console.log('currentposition',htmlElement);
+//     let children = htmlElement.children;
+//     if (children.length !==0){
+//         for (const child of children) {
+//             console.log('inner elements',child);
+//         }
+//     }
+// }
+//
+// explorer(document.body);
+
+// function explorer(htmlElement) {
+//     console.log(htmlElement);
+//     let children = htmlElement.children;
+//     if (children.length !==0){
+//         for (const child of children) {
+//             explorer(child);
+//         }
+//     }
+// }
+//
+// explorer(document.body);
+
+
+
+///repeat
+
+function explorer(htmlElement) {
+    console.log(htmlElement);
+    let children = htmlElement.children;
+    if (children.length !== 0){
+        for (const child of children) {
+            // console.log(child);
+            explorer(child);
+        }
+    }
+
+}
+
+explorer(document.body)
+
+
+
+
+
+
+
+
+
 
 
 
