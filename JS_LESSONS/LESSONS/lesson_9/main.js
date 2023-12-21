69c173ea63f1060c62c5f1b503b5cd5156e1e061
Sasha-Makar <makarsasha7@gmail.com>
2023-12-21 15:11:48 +0200

    add lesson_9-js(add some code)

--- JS_LESSONS/LESSONS/lesson_9/main.js
+++ JS_LESSONS/LESSONS/lesson_9/main.js
@@ -1,15 +1,15 @@
 
-let obj = {
-    key1 : 'value'
-}
-
-function ObjCreator(value1) {
-    this.key1 = value1;
-}
-
-let object = new ObjCreator('qweasd');
-
-console.log(object);
+// let obj = {
+//     key1 : 'value'
+// }
+//
+// function ObjCreator(value1) {
+//     this.key1 = value1;
+// }
+//
+// let object = new ObjCreator('qweasd');
+//
+// console.log(object);
 
 // function explorer(element) {
 //     console.log(element);
@@ -22,3 +22,47 @@ console.log(object);
 //     }
 // }
 // explorer(document.body)
+
+// function explorer(element) {
+//     console.log(element);
+//     let children = element.children;
+//     if (children.length !== 0){}
+//     for (const child of children) {
+//         explorer(child);
+//     }
+// }
+
+// explorer(document.body);
+
+
+// console.log(document);
+// document.title = 'hello okten';
+
+
+// let divWrap = document.getElementsByClassName('wrap');
+// console.log(divWrap);
+let wrap = document.getElementsByClassName('wrap')[0];
+console.log(wrap);
+
+let pointList = wrap.getElementsByClassName('point');
+console.log(pointList);
+
+let target = document.getElementById('target');
+console.log(target);
+
+let h1 = document.getElementsByTagName('h1');
+console.log(h1);
+
+
+console.log(wrap.children);
+
+for (const child of wrap.children) {
+    console.log(child.innerText);
+    child.style.background = 'blue';
+    // child.innerText = 'XXX';
+}
+
+
+
+let point = document.querySelectorAll('.point');
+console.log(point);
