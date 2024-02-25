8969507c01af4238e2047e8592539dfcb7b095bc
Sasha-Makar <makarsasha7@gmail.com>
2024-02-26 01:39:49 +0200

    add repeat_lesson_9-JS

---
+++ JS_LESSONS/LESSONS/lesson_9/repeat/main.js
@@ -0,0 +1,49 @@
+// console.log(22);
+
+
+// let s = {
+//     name: 'asd'
+// }
+// console.log(s);
+//
+//
+// function ObjCreator(value) {
+//     this.key = value;
+// }
+//
+// let d = new ObjCreator('vasya');
+// console.log(d);
+
+
+console.log(window);
+console.log(window.document);
+
+console.log(document.title);
+document.title = 'hello Okten';
+console.log(document.title);
+
+
+console.log('//////////////');
+
+
+let element1 = document.querySelector('.point');
+console.log(element1);
+
+
+let allEl = document.querySelectorAll('.point');
+console.log(allEl);
+
+let divEl = document.getElementsByClassName('point');
+console.log(divEl);
+
+
+
+
+
+
+
+
+
+
+
+
